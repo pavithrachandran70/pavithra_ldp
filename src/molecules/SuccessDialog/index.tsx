50ffe09a5b7bbc08c6c7116ae85ee5202135626c
pavithrachandran70 <pavithrazemosolabs.com>
2025-06-06 15:32:35 +0530

    updated the changes

--- src/molecules/SuccessDialog/index.tsx
+++ src/molecules/SuccessDialog/index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import sucess from "../../assets/sucess.png"
+import { SUCCESS_MODAL_TEXTS } from '../../utils/constants';
 import {
 
   DialogTitle,
@@ -24,31 +25,31 @@ interface SuccessModalProps {
 
 const SuccessModal: React.FC<SuccessModalProps> = ({ isOpen, onClose, onViewCashKicks }) => {
   return (
-    <StyledDialog open={isOpen} onClose={onClose} fullWidth maxWidth="sm">
-      <DialogTitle>Cash kick launched successfully!</DialogTitle>
+     <StyledDialog open={isOpen} onClose={onClose} fullWidth maxWidth="sm">
+      <DialogTitle>{SUCCESS_MODAL_TEXTS.dialogTitle}</DialogTitle>
       <DialogContent>
         <TypographyAtom variant="subtitle1" gutterBottom>
-          We are reviewing your cash kick
+          {SUCCESS_MODAL_TEXTS.subtitle}
         </TypographyAtom>
 
         <CenteredImageBox>
           <img src={sucess} alt="Success" width="100" />
         </CenteredImageBox>
 
-        <CenteredText variant="h6">Your cash kick is under review</CenteredText>
-        <CenteredText variant="body2" >
-          It will remain in a pending state until we review it internally.
-          This can take up to 5 minutes or a couple of hours. Once reviewed,
-          the cash will be transferred to your account and you'll be notified.
+        <CenteredText variant="h6">
+          {SUCCESS_MODAL_TEXTS.mainHeading}
+        </CenteredText>
+        <CenteredText variant="body2">
+          {SUCCESS_MODAL_TEXTS.description}
         </CenteredText>
       </DialogContent>
 
       <CustomDialogActions>
         <Button variant="outlined" onClick={onClose}>
-          Close
+          {SUCCESS_MODAL_TEXTS.closeButton}
         </Button>
         <Button variant="contained" onClick={onViewCashKicks}>
-          View Cash Kicks
+          {SUCCESS_MODAL_TEXTS.viewCashKicksButton}
         </Button>
       </CustomDialogActions>
     </StyledDialog>
--- src/molecules/SumaryCard/index.tsx
+++ src/molecules/SumaryCard/index.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { CardContent, Typography, Box } from '@mui/material';
 import * as Styled from '../SumaryCard/index.styles'
+import ifoicon from '../../assets/icon.svg';
+import TypographyAtom from '../../atoms/Typography';
 
 interface SummaryCardProps {
   term: string;
@@ -24,43 +26,39 @@ const SummaryCard: React.FC<SummaryCardProps> = ({
         <Styled.SummaryHeader variant="h5" gutterBottom>
           <span>Summary</span>
           <Box mt={0.5}>
-            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10.0003 18.8334C14.5837 18.8334 18.3337 15.0834 18.3337 10.5001C18.3337 5.91675 14.5837 2.16675 10.0003 2.16675C5.41699 2.16675 1.66699 5.91675 1.66699 10.5001C1.66699 15.0834 5.41699 18.8334 10.0003 18.8334Z" stroke="#727080" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-              <path d="M9.99805 13.8333L9.99805 9.66659" stroke="#727080" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-              <path d="M10.0029 7.16675L9.99544 7.16675" stroke="#727080" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
+            <img src={ifoicon} alt="IFO Icon" width="24" height="24" />
           </Box>
         </Styled.SummaryHeader>
 
         <Styled.InfoContainer>
           <Styled.Row>
-            <Typography variant="body1">Term:</Typography>
-            <Typography variant="body1" align="right">{term}</Typography>
+            <TypographyAtom variant="body1">Term:</TypographyAtom>
+            <TypographyAtom variant="body1" align="right">{term}</TypographyAtom>
           </Styled.Row>
 
           <Styled.Row>
-            <Typography variant="body1">Selected Contracts:</Typography>
-            <Typography variant="body1" align="right">{selectedContracts}</Typography>
+            <TypographyAtom variant="body1">Selected Contracts:</TypographyAtom>
+            <TypographyAtom variant="body1" align="right">{selectedContracts}</TypographyAtom>
           </Styled.Row>
 
           <Styled.Row>
-            <Typography variant="body1">Pay Back Amount:</Typography>
-            <Typography variant="body1" align="right">{payBackAmount}</Typography>
+            <TypographyAtom variant="body1">Pay Back Amount:</TypographyAtom>
+            <TypographyAtom variant="body1" align="right">{payBackAmount}</TypographyAtom>
           </Styled.Row>
 
           <Styled.Row>
-            <Typography variant="body1">Rate:</Typography>
-            <Typography variant="body1" align="right">{rate}</Typography>
+            <TypographyAtom variant="body1">Rate:</TypographyAtom>
+            <TypographyAtom variant="body1" align="right">{rate}</TypographyAtom>
           </Styled.Row>
         </Styled.InfoContainer>
 
         <Styled.StyledDivider />
 
         <Box mt={2}>
-          <Typography variant="h6" fontWeight="bold">
+          <TypographyAtom variant="h6" fontWeight="bold">
             Total Payout:{' '}
             <Styled.TotalPayout>{totalPayout}</Styled.TotalPayout>
-          </Typography>
+          </TypographyAtom>
         </Box>
       </CardContent>
     </Styled.StyledCard>
